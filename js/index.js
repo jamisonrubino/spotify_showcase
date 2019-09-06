f3c9065c379f55824d6b075c70759cb95b386546
jamisonrubino <jamison.rubino@gmail.com>
2019-09-06 10:59:13 -0700

    +comments

--- js/index.js
+++ js/index.js
@@ -8,24 +8,35 @@ var usersHTML = "",
 	playlists,
 	accessToken,
 	usersDiv = document.getElementById('users'),
-	musicDiv = document.getElementById('music'),
+	playlistsDiv = document.getElementById('playlists'),
 	loginDiv = document.getElementById('login'),
 	webPlayer = document.getElementById('webplayer');
 
+
+//====================================
+//======================LOGIN REDIRECT
+
+// triggered by 'login' button click
 function authorize() {
 	var scopes = 'streaming user-read-email user-follow-read';
 	window.location.replace('https://accounts.spotify.com/authorize' +
 	'?response_type=token' + '&client_id=' + clientID + (scopes ? '&scope=' + encodeURIComponent(scopes) : '') + '&redirect_uri=' + encodeURIComponent(redirectURI)) + '&state=123';
 }
 
+
+//==========================================
+//======================PAGE LOAD AUTH CHECK
+
+// if redirected here with access_token but cookie not set, set cookie value
 if (window.location.href.indexOf('access_token') > -1 && getCookie('access_token').length == 0)
 	setAuth();
 
+// if cookie set, hide login button, begin info fetching and make HTML sections visible
 if (getCookie('access_token').length > 0) {
 	fetchUsers();
 	loginDiv.style.display = "none";
 	usersDiv.style.display = "inline-block";
-	musicDiv.style.display = "inline-block";
+	playlistsDiv.style.display = "inline-block";
 	webPlayer.style.display = "inline-block";
 } else {
 	loginDiv.style.display = "block";
@@ -33,29 +44,36 @@ if (getCookie('access_token').length > 0) {
 
 
 
-//=============================
-//==============FETCH FUNCTIONS
+//=====================================
+//======================FETCH FUNCTIONS
 
-// fetch all user objects
+// call fetchUser for each user in users list
+// fetchUser queries Spotify for the user's public info and returns it here
+// add new 'div' block to usersHTML for current user
 async function fetchUsers() {
 	for (let i=0; i<users.length; i++) {
 	 	await fetchUser(`${users[i]}`);
-		console.log(user)
 		usersHTML += `<div class="user" onclick="fetchPlaylists('${user.id}')">${user.display_name}</div>`;
 	}
 	usersDiv.innerHTML = usersHTML;
 }
 
-// fetch a single user object
+// query Spotify for a user's public info
+// convert the returned string to an object with .json()
+// then assign object to global var "user"
 function fetchUser(selectedUser) {
 	return fetch(`https://api.spotify.com/v1/users/${selectedUser}`, {headers: {'Authorization': `Bearer ${getCookie('access_token')}`}})
 		.then(response => response.json())
 		.then(userRes => user = userRes)
 }
 
-// fetch a list of playlists
+// reset playlistsDiv's HTML to an empty string
+// query Spotify for a user's public playlists
+// convert json string server response to object
+// for each playlist, add clickable div to playlistsHTML with its info
+// update playlistsDiv's HTML to fit the new playlists list
 function fetchPlaylists(selectedUser) {
-	musicDiv.innerHTML = "";
+	playlistsDiv.innerHTML = "";
 	playlists = null;
 	playlistsHTML = "";
 	fetch(`https://api.spotify.com/v1/users/${selectedUser}/playlists`, {headers: {'Authorization': `Bearer ${getCookie('access_token')}`}})
@@ -64,21 +82,21 @@ function fetchPlaylists(selectedUser) {
 			playlists = res.items;
 			for (let i=0; i<playlists.length; i++) {
 				playlistsHTML += `<div class="playlist" onclick="loadPlaylist('${playlists[i].id}')">${playlists[i].name} - ${playlists[i].tracks.total}</div>`;
-				console.log("playlist", playlists[i])
 			}
-			musicDiv.innerHTML = playlistsHTML;
+			playlistsDiv.innerHTML = playlistsHTML;
 		})
 }
 
-// load a playlist into Spotify web player
+// accept a playlist id
+// incorporate playlist ID in webplayer's URL and reload webplayer
 function loadPlaylist(playlist) {
 	webPlayer.innerHTML = `<iframe src="https://open.spotify.com/embed/playlist/${playlist}" width="350" height="480" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`;
 }
 
 
 
-//=====================================
-//==============GET AND SET AUTH COOKIE
+//=============================================
+//======================GET AND SET AUTH COOKIE
 
 function setAuth() {
 	var url = window.location.href
